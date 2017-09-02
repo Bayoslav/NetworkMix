38b639351cf307031a8c38d1c16438ab34e3dc0a
Filip Lav Maksimovic <gaming4ever93@gmail.com>
2017-09-03 00:25:00 +0200

    Update forms.py

--- all_app/forms.py
+++ all_app/forms.py
@@ -5,6 +5,12 @@ class UserForm(forms.ModelForm):
     #url = forms.URLField(label='Your website', required=False)
     email = forms.CharField(required=True, widget=forms.TextInput(attrs={'class' : 'uk-input uk-width-1-2', 'style' : 'width: 100%; margin: 20px 0px; border-radius: 10px;'}))
     password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class' : 'uk-input uk-width-1-2', 'style' : 'width: 100%; margin: 20px 0px; border-radius: 10px;'}))
+    def save(self, commit=True):
+        user = super(UpForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
     class Meta:
         model = User
         fields = ("username", "email", "password")
