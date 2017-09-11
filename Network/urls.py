82399ee40760f56592dacc9cceb70f966d568a8d
Pavle Vojinovic <vv.pavle@gmail.com>
2017-09-11 20:16:46 +0200

    Dashboard Redesign

--- Network/urls.py
+++ Network/urls.py
@@ -21,8 +21,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', Home.as_view()),
-    url(r'login/$', LogIn.as_view()),
-    url(r'dashboard/$', Dashboard.as_view(), name='dashboard'),
+    url(r'^login/$', LogIn.as_view()),
+    url(r'^dashboard/$', Dashboard.as_view(), name='dashboard'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
